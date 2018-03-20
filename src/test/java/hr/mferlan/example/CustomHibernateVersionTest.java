b0ceedec06b88c1e04952eaf2b1b91c29967924d
Martin Ferlan <martinferlan@atron.de>
2018-03-20 09:06:55 +0100

    Revert "Patching connector with custom hibernate version"
    This reverts commit dd65df72f6da0568c81c04815ca8928744acb875.
    
    Reverting changes, this should have been pushed to separate branch

--- src/test/java/hr/mferlan/example/CustomHibernateVersionTest.java
+++ src/test/java/hr/mferlan/example/CustomHibernateVersionTest.java
@@ -3,15 +3,18 @@ package hr.mferlan.example;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.inject.Inject;
 
 import org.hibernate.Session;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.transaction.api.annotation.TransactionMode;
 import org.jboss.arquillian.transaction.api.annotation.Transactional;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.wildfly.swarm.arquillian.DefaultDeployment;
 
 /**
  * Integration test for using the custom Hibernate ORM version on WildFly Swarm.
@@ -20,25 +23,26 @@ import org.wildfly.swarm.arquillian.DefaultDeployment;
 @DefaultDeployment
 public class CustomHibernateVersionTest {
 
-    /** The entity manager. */
-    @Inject
-    private PersistenceHelper helper;
+	/** The entity manager. */
+	@Inject
+	private PersistenceHelper helper;
 
-    @Test
-    @Transactional(value = TransactionMode.ROLLBACK)
-    public void shouldUseHibernateOrm53() {
-        Session session = this.helper.getEntityManager().unwrap(Session.class);
 
-        MyEntity entity1 = new MyEntity();
-        entity1.id = 1L;
-        entity1.description = "Some MyEntity";
-        session.persist(entity1);
+	@Test
+	@Transactional(value=TransactionMode.ROLLBACK)
+	public void shouldUseHibernateOrm53() {
+		Session session = helper.getEntityManager().unwrap( Session.class );
 
-        session.flush();
-        session.clear();
+		MyEntity entity1 = new MyEntity();
+		entity1.id = 1L;
+		entity1.description = "Some MyEntity";
+		session.persist( entity1 );
 
-        MyEntity loaded = session.find(MyEntity.class, 1L);
+		session.flush();
+		session.clear();
+		
+		MyEntity loaded = session.find( MyEntity.class, 1L );
 
-        assertThat(loaded.description, equalTo("Some MyEntity"));
-    }
+		assertThat( loaded.description, equalTo( "Some MyEntity" ) );
+	}
 }
